a5af0d7aecd4c35d44984e35c4182118e11d750b
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2018-09-02 22:08:52 +0300

    1.7-1.10 Done

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            init: false,
             stats: {
                 hyva: 0,
                 neutraali: 0,
@@ -19,6 +20,7 @@ class App extends React.Component {
         newStats[type] = this.state.stats[type] + 1 
 
         this.setState({
+            init: true,
             stats: newStats
         })
     }
@@ -31,7 +33,7 @@ class App extends React.Component {
                 <Button text={"neutraali"} handleClick={this.addPalaute("neutraali")}/>
                 <Button text={"huono"} handleClick={this.addPalaute("huono")}/>
                 <Header text={"statistiikka"}/>
-                <Statistics stats={this.state.stats}/>
+                {this.state.init === false ? (<p>ei yhtään palautetta annettu</p>) : <Statistics stats={this.state.stats}/>}
             </div>
         )
     }
@@ -39,14 +41,29 @@ class App extends React.Component {
 
 const Header = ({text}) => (<h1>{text}</h1>)
 const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
-const Statistics = ({stats}) => (
-    <div>
-        {
-            Object.keys(stats).map((key, id) => (
-                <p key={id}>{key} {stats[key]}</p>
-            )) 
-        }
-    </div>
-)
+const Statistics = ({stats}) => {
+    const getKeskiarvo = () => getTotal() === 0 ? 0 : (stats.hyva * 1 + stats.neutraali * 0 + stats.huono * (-1)) / getTotal()
+    const getPositiiviset = () =>  getTotal() === 0 ? 0 : stats.hyva / getTotal() * 100 
+    const getTotal = () => Object.keys(stats).reduce((acc, val) => acc + stats[val], 0);
+
+    return (
+        <div>
+            {
+                Object.keys(stats).map((key, id) => (
+                    <p key={id}>{key} {stats[key]}</p>
+                )) 
+            }
+            <Statistic teksti={"keskiarvo"} arvo={getKeskiarvo()}/>
+            <Statistic teksti={"positiiviset"} arvo={getPositiiviset() + " %"}/>
+        </div>
+    )
+}
+const Statistic = ({teksti, arvo}) => {
+    return (
+        <p>
+            {teksti} {arvo}
+        </p>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
